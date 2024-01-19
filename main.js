225e67a10e07a2ce6422cae3a79bb1104bcbb1b8
Mark Farmiloe <mark@ccsdev.net>
2024-01-19 23:18:37 +0000

    More work on table loading and style

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-const setDisplay = (el, value) => {
-    el.setAttribute("display", value);
-}
-
+// Load navbar
 const buildMenuItem = item => {
     const li = document.createElement("li");
     li.addEventListener("mouseover", (e) => {
@@ -36,12 +33,20 @@ const loadNav = data => {
     n.insertBefore(ul, fc);
 }
 
+// Load grid
 const toggleSort = (e) => {
     const th = e.target;
     console.log(th);
-    const xs = th.attributes['x-sort']?.value 
+    const xs = th.attributes['x-sort']?.value
     console.log(xs);
     th.setAttribute('x-sort', (xs === "asc") ? "desc" : "asc");
+}
+
+const emptyTable = t => {
+    // do we need to remove the eventlisteners first?
+    while (t.firstChild) {
+        t.firstChild.remove();
+    }
 }
 
 const buildTHead = fldInfos => {
@@ -59,7 +64,7 @@ const buildTHead = fldInfos => {
 }
 
 const dateToString = v => {
-    const d = new Date(0,0,v);
+    const d = new Date(0, 0, v - 1);
     return d.toLocaleDateString();
 }
 
@@ -98,19 +103,20 @@ const buildTBody = (fldInfos, dataRows) => {
     return tbody;
 }
 
-const loadGrid = data => {
-    console.log(data[0]);
-    console.log(data[1][0]);
+const loadGrid = key => {
+    data = dataStore[key];
+    // console.log(data[0]);
+    // console.log(data[1][0]);
     const t = document.getElementById("data");
+    emptyTable(t);
     const thead = buildTHead(data[0]);
     t.appendChild(thead);
     const tbody = buildTBody(data[0], data[1]);
     t.appendChild(tbody);
 }
 
-const myRequest = new Request("nav.json");
-
-fetch(myRequest)
+// Fetch data
+fetch(new Request("nav.json"))
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
@@ -121,15 +127,29 @@ fetch(myRequest)
         loadNav(data);
     });
 
-const dataRequest = new Request("Customers.json");
+const dataStore = {};
 
-fetch(dataRequest)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        loadGrid(data);
-    });
+const fetchToDataStore = async (key, path, loadToGrid) => {
+    const response = await fetch(path);
+    const data = await response.json();
+    dataStore[key] = data; 
+    if (loadToGrid) loadGrid(key);
+}  
+
+fetchToDataStore("Customers", "Customers.json", true);
+fetchToDataStore("Suppliers", "Suppliers.json", false);
+
+setTimeout(() => loadGrid("Suppliers"), 5000);
+
+// const dataRequest = new Request("Customers.json");
+// fetch(dataRequest)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         dataStore.Customers = data;
+//         loadGrid(data);
+//     });
